86de81dabaa722a66ce67e758d9f6aef03ce2bbf
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2023-11-03 12:08:28 -0300

    Create Subtração.java

---
+++ Lista 1/Subtração.java
@@ -0,0 +1,16 @@
+// 1. Faça um programa que receba dois números, calcule e mostre a subtração do primeiro número pelo segundo.
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    double N1, N2, RES;
+	    
+	  N1 = Double.parseDouble(System.console().readLine());
+      N2 = Double.parseDouble(System.console().readLine());
+
+RES = (N1-N2);
+
+		System.out.println(RES);
+	}
+}
