3832af23e085145181c3043216568226a9a6149e
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2024-02-22 21:10:43 -0300

    Update Divisíveis.java

--- Lista 2/Divisíveis.java
+++ Lista 2/Divisíveis.java
@@ -1,3 +1,9 @@
+/******************************************************************************
+5. Desenvolva um programa que leia quatro valores numéricos inteiros e
+mostre os valores que são divisíveis por 2 e 3.
+*******************************************************************************/
+
+
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +20,54 @@ public class Main {
             System.out.println("Os valores são divisíveis por 2 e 3.");
         } else {
             System.out.println("Os valores informados não são divisíveis por 2 e 3.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean divisivel(int num) {
+        return num % 2 == 0 && num % 3 == 0;
+    }
+}
+
+/******************************************************************************
+6. Desenvolva um programa que leia quatro valores numéricos inteiros e
+mostre os valores que são divisíveis por 2 e 3.
+*******************************************************************************/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite 4 valores: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        
+        boolean divisivelA = divisivel(a);
+        boolean divisivelB = divisivel(b);
+        boolean divisivelC = divisivel(c);
+        boolean divisivelD = divisivel(d);
+        
+        if (divisivelA || divisivelB || divisivelC || divisivelD) {
+            System.out.println("Os valores divisíveis por 2 e 3 são:");
+            if (divisivelA) {
+                System.out.println(a);
+            }
+            if (divisivelB) {
+                System.out.println(b);
+            }
+            if (divisivelC) {
+                System.out.println(c);
+            }
+            if (divisivelD) {
+                System.out.println(d);
+            }
+        } else {
+            System.out.println("Nenhum dos valores é divisível por 2 e 3.");
         }
 
         scanner.close();
